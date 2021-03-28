2d2e88ad37e11b8c4d485de9a692b957ebbdb18b
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2021-03-28 20:07:24 +1300

    Some tiding incl index.html

--- js/auth.js
+++ js/auth.js
@@ -117,6 +117,13 @@ async function requestApiToken() {
     }
 }
 
+async function getAuthenticatedUser() {
+    const apiToken = await getApiToken();
+    if (apiToken) {
+        return await Api.getAuthenticatedUser(apiToken)
+    } 
+    return null;
+}
 
 //Below this line is some code pulled from https://github.com/aaronpk/pkce-vanilla-js
 /*
@@ -133,6 +140,14 @@ furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 //////////////////////////////////////////////////////////////////////
 // PKCE HELPER FUNCTIONS
