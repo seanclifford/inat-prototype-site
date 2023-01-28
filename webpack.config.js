9fe11b13a7b5aa7c9b730d1fbcb30199038efae1
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2023-01-28 21:56:31 +1300

    Add babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,20 @@ module.exports = {
     path: `${__dirname}/js/dist`,
     filename: '[name].js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
 };
