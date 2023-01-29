b4c7430f7fe58bb74b62d1d9784edbc9459fa692
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2023-01-29 14:15:39 +1300

    add webpack entry points for other pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   entry: {
     bulk_send_message: './js/bulk_send_message.js',
-    auto_combine_suggest: './js/auto_combine_suggest.js'
+    auto_combine_suggest: './js/auto_combine_suggest.js',
+    oauth_redirect: './js/oauth_redirect.js',
+    site_selection: './js/site_selection.js',
+    auth_test: './js/auth_test.js',
   },
   mode: 'development',
   output: {
@@ -11,13 +14,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
+              ['@babel/preset-env', { targets: "defaults" }] //https://browsersl.ist/#q=defaults
             ]
           }
         }
