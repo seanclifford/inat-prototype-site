363342a96549c7a41dc43f45b33d880104845f90
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2023-02-12 16:50:08 +1300

    React - First trial with HtmlWebpackPlugin
    Not creating anything usable yet. Just proving out the tooling.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: {
     bulk_send_message: './js/bulk_send_message.js',
     auto_combine_suggest: './js/auto_combine_suggest.js',
+    auto_combine_suggest2: './src/auto_combine_suggest2.js',
     oauth_redirect: './js/oauth_redirect.js',
     site_selection: './js/site_selection.js',
     auth_test: './js/auth_test.js',
@@ -9,6 +12,7 @@ module.exports = {
   output: {
     path: `${__dirname}/../js/dist`,
     filename: '[name].js',
+    clean: true
   },
   module: {
     rules: [
@@ -30,5 +34,13 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.ejs',
+      title: 'iNat prototypes - Auto Combine Observations PROTOTYPE',
+      filename: 'auto_combine_suggest2.html',
+      chunks: ['auto_combine_suggest2']
+    })
+  ]
 };
---
+++ src/AutoCombineSuggest.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./AutoCombineSuggest.css"
+
+export default function AutoCombineSuggest() {
+    return (
+        <div>
+            <h1>Auto Combine Observations prototype for iNaturalist</h1> 
+        
+            This tool creates a suggestion link for an owner of some observations to automatically combine them into a single observation.<br/>
+            This can be useful when finding new users that have not got the hang of the web uploader yet and end up creating multiple observations accidentally.
+        </div>
+    );
+}
